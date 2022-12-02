234606a90306c42726e013933330ecf6339e596c
Philippe De Pauw - Waterschoot <phildp@MACINTOSH.local>
2022-12-02 14:32:31 +0100

    Added forms to the website

---
+++ opdracht_01/data/references.js
@@ -0,0 +1,14 @@
+const references = [
+  {
+    title: 'Huisje in de Ardennen',
+    pictureURL: 'assets/images/private_1.jpg',
+  },
+  {
+    title: 'Opgehoogde tuin in Denemarken',
+    pictureURL: 'assets/images/private_2.jpg',
+  },
+  {
+    title: 'Tuin voor klant in Ibiza',
+    pictureURL: 'assets/images/private_3.jpg',
+  }
+];
--- opdracht_01/js/main.js
+++ opdracht_01/js/main.js
@@ -1,3 +1,47 @@
 (() => {
+  const app = {
+    init () {
+      console.log('1. Initialize Application.');
+      this.cacheElements();
+      this.registerListeners();
+    },
+    cacheElements () {
+      console.log('2. Cache Elements.');
+      this.$referencesList = document.querySelector('.references-list');
+      this.$referencesList.innerHTML = this.getHTMLForReferences(references);
+      this.$fromContact = document.querySelector('#fromContact');
+    },
+    registerListeners () {
+      console.log('2. Register Listeners.');
+      this.$fromContact.addEventListener('submit', (ev) => {
+        ev.preventDefault();
 
+        const $elements = ev.currentTarget.elements;
+
+        const data = {
+          type: $elements.type.value,
+          gender: $elements.gender.value,
+          name: $elements.txtName.value,
+        }
+
+        const dataAsJSONStr = JSON.stringify(data);
+        console.log(dataAsJSONStr);
+
+        return false;
+      });
+    },
+    getHTMLForReferences (references) {
+      return references.map((ref) => {
+        return `
+          <div class="reference">
+            <h3 class="reference__title">${ref.title}</h3>
+            <picture class="reference__picture">
+              <img src="${ref.pictureURL}">
+            </picture>
+          </div>
+        `;
+      }).join('');
+    }
+  };
+  app.init();
 })();
